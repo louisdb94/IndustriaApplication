d7311b71d28d6e658b46edb45b3d190679b4406a
louisdb94 <dalbosco.louis@gmail.com>
2017-10-10 21:13:25 +0200

    profilepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,18 +17,28 @@ import { AppState } from './app.service';
     './app.component.scss'
   ],
   template: `
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"> <img src="../assets/img/Industria.png" width="30px" height="40px"> Industria </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <nav class="navbar navbar-fixed-top navbar-dark bg-inverse" >
-    <div class="container">
-        <a class="navbar-brand">Angular Router</a>
-        <ul class="nav navbar-nav" routerLinkActive="active">
-          <li class="nav-item"><a class="nav-link" routerLink="home" routerLinkActive="active">Home</a></li>
-
-        </ul>
-    </div>
-</nav>
-
-
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Job Openings</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
 
       <router-outlet></router-outlet>
     <footer>
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    
+  email =  "dalbosco.louis@gmail";
+  phone =  "0475634728";
 }
